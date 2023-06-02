b95b0fa1bbdc1aacc7a19ecdb49bbb5a1f7fe9a0
Nazmul Hossain <50129825+nazmulice@users.noreply.github.com>
2023-06-03 02:17:07 +0600

    Delete .history directory

--- .history/models/live_20230601231331.js
+++
@@ -1,7 +0,0 @@
-
-//Create a user schema using Mongoose with the following requirements:
-Name field: Required, string type, minimum length of 3 characters
-Email field: Required, unique, string type, 
-Age field: Optional, number type, minimum value of 18
-Password field: Required, string type, minimum length of 6 characters
-
